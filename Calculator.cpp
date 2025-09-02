ae89196752ee5e13f3899cc9c56a532cc65f32a7
D1-92785-Akash <akashjagtap487@gmail.com>
2025-09-02 15:37:48 +0530

    added ADD functionality

---
+++ Calculator.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int menuList() {
+    int choice;
+    cout<<"0 - Exit"<<endl;
+    cout<<"1 - ADD"<<endl;
+    cout<<"Enter your choice : "<<endl;
+    cin>>choice;
+    return choice;
+}
+
+int main() {
+    int choice,n1,n2,res;
+    while((choice=menuList())!=0) {
+        cout<<"Enter two numbers : ";
+        cin>>n1>>n2;
+        switch (choice)
+        {
+        case 1:
+            res=n1+n2;
+            cout<<"ADD result = "<<res<<endl;
+            break;
+        default:
+            break;
+        }
+    }
+}
